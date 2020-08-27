b97852218429b9069b8a76a07430fefcc9e661e2
JHerng <jiaherng2001@gmail.com>
2020-08-27 15:48:57 +0800

    second commit

--- src/GitCommandLine.java
+++ src/GitCommandLine.java
@@ -1,5 +1,12 @@
 public class GitCommandLine {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.print("First step:");
+        System.out.println("add the file which changes occurred");
+        System.out.print("Second step:");
+        System.out.println("commit the file and insert commit message");
+        System.out.print("Third step:");
+        System.out.println("push the file");
+        System.out.println("That's all");
     }
 }
